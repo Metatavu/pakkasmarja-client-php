7afc5bed05bd4083a39af1a541ecdb208c720aa4
Antti Leppä <antti.leppa@metatavu.fi>
2018-04-03 13:01:00 +0300

    Minor update

--- lib/Api/ContractsApi.php
+++ lib/Api/ContractsApi.php
@@ -284,7 +284,7 @@ class ContractsApi
             );
         }
 
-        $resourcePath = '/contacts';
+        $resourcePath = '/contracts';
         $formParams = [];
         $queryParams = [];
         $headerParams = [];
